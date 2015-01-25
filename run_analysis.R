c8117ca9ad69c62f81ae0597f9d363287d7b59dc
unknown <svadlama@TXW184220.informatica.com>
2015-01-25 13:38:34 -0600

    submit

--- run_analysis.R
+++ run_analysis.R
@@ -2,12 +2,19 @@ setwd("C:/UCIHARDataset/")
 
 #Reading Test Data
 test_data <- read.table("C:/UCIHARDataset/test/X_test.txt")
-subset_cols <- read.table("C:/UCIHARDataset/subset.txt")
-data_desc <- subset_cols[,2]
+#subset_cols <- read.table("C:/UCIHARDataset/subset.txt")
+#data_desc <- subset_cols[,2]
+
+features<- read.table("C:/UCIHARDataset/features.txt")
+featureList <-as.character(features[,2])
+featureList <- make.names(featureList, unique=TRUE)
+colnames(test_data) <- featureList
 
 #subsetting the merged_test_data with the columns having mean and std.
 subset_test_data  <- test_data[,c(1:6, 41:46, 81:86, 121:126, 161:166, 201:202, 214:215, 227,228,240,241,253,254,266,267,268,269,270,271,345,346,347,348,349,350,424,425,426,427,428,429,503,504,516,517,529,530,542,543)]
-colnames(subset_test_data) <- data_desc
+#colnames(subset_test_data) <- data_desc
+#colnames(subset_test_data)
+
 
 
 #Reading the activity labels data and the description and merging
@@ -29,13 +36,12 @@ merged_test_data <- cbind(subject_test, y_test_activity_description, subset_test
 
 ####Reading train Data
 train_data <- read.table("C:/UCIHARDataset/train/X_train.txt")
-subset_cols <- read.table("C:/UCIHARDataset/subset.txt")
-data_desc <- subset_cols[,2]
-nrow(train_data)
+
+colnames(train_data) <- featureList
 
 #subsetting the merged_train_data with the columns having mean and std.
 subset_train_data  <- train_data[,c(1:6, 41:46, 81:86, 121:126, 161:166, 201:202, 214:215, 227,228,240,241,253,254,266,267,268,269,270,271,345,346,347,348,349,350,424,425,426,427,428,429,503,504,516,517,529,530,542,543)]
-colnames(subset_train_data) <- data_desc
+#colnames(subset_train_data) <- data_desc
 
 
 #Reading the activity labels data and the description and merging
@@ -54,10 +60,15 @@ colnames(subject_train) <- c("Subject")
 
 #Merging the 3data sets -- subject, activity description and the train data
 merged_train_data <- cbind(subject_train, y_train_activity_description, subset_train_data)
-nrow(merged_train_data)
 
+
+
+#Merging Test and train data
 merged_data_all <- rbind(merged_test_data, merged_train_data)
-tidy_data <- 
-  average()
-  group_by (subject, activity)
-colnames(merged_data_all)
+before_tidy <- merged_data_all[,-2]
+
+
+tidy <-aggregate(before_tidy[,3:68], by=list(merged_data_all$Subject,merged_data_all$activity_description), 
+                    FUN=mean, na.rm=TRUE)
+
+write.table(tidy, "C:/tidy.txt", row.name=FALSE)
